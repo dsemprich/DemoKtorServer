cae2ad7c239270d4dd4018e71b0afd483cbda7b9
david.semprich <david.semprich@hella-gutmann.com>
2022-02-15 14:36:06 +0100

    auto reload

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,11 +1,9 @@
 package com.example.plugins
 
 import io.ktor.application.*
-import io.ktor.html.*
 import io.ktor.http.content.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import kotlinx.html.*
 import kotlinx.serialization.Serializable
 
 fun Application.configureRouting() {
@@ -18,26 +16,8 @@ fun Application.configureRouting() {
             call.respondText("Hello World!")
         }
         get("/welcome") {
-            val name = call.request.queryParameters["name"]
-            call.respondHtml {
-                head {
-                    title {
-                        +"Custom Title"
-                    }
-                }
-                body {
-                    if (name.isNullOrEmpty()) {
-                        h3 { +"Welcome!" }
-                    } else {
-                        h3 { +"Welcome, $name" }
-                    }
-                    p { +"Current directory is: ${System.getProperty("user.dir")}" }
-                    img(src = "logo.png")
-                }
-            }
+            call.respondText(" Welcome!")
         }
-
-
     }
 }
 
